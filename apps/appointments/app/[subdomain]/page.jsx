65f7f3217b8012ea5f474261299b418e4d8335f3
bsfranca2 <bsfranca2@gmail.com>
2023-04-04 01:10:27 -0300

    force build

--- apps/appointments/app/[subdomain]/page.jsx
+++ apps/appointments/app/[subdomain]/page.jsx
@@ -1,7 +1,8 @@
 export default function Page({ params }) {
   return (
     <div>
-      <h1>Page</h1>
+      <h1>Barbearia.io</h1>
+      <h2>Page</h2>
       <p>Subdomain: {params.subdomain}</p>
     </div>
   );
